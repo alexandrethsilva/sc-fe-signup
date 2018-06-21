f8d24404cc2e58f7fc78ef5f25f5cc6115408478
Alexandre Theodoro da Silva <alexandrethsilva@users.noreply.github.com>
2018-06-21 22:44:44 +0200

    Update index.js

--- application/constants/index.js
+++ application/constants/index.js
@@ -1,3 +1,3 @@
-export const API_ENDPOINT = "http://api.spotcap.com:4001"
-export const API_ENDPOINT_SIGNUP = "http://api.spotcap.com:8485/identity/user/signup"
+export const API_ENDPOINT = "http://api.server:4001"
+export const API_ENDPOINT_SIGNUP = "http://api.server:8485/identity/user/signup"
 export const API_ENDPOINT_COUNTRIES = "https://restcountries.eu/rest/v2/all?fields=name;alpha2Code"
